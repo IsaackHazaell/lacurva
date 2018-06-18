8762feb523537c3c99420be3ab073c079c491382
IsaackHazaell <isaachvc@hotmail.com>
2018-06-18 16:20:21 -0500

    Se agregaron imagenes y se añadio información de contacto

--- js/map-custom.js
+++ js/map-custom.js
@@ -202,8 +202,8 @@
                 map_zoom = 14;
 
             var locations = [
-                ['<div class="infobox"><h4>Pato</h4><p>Now that you visited our website, how' +
-                ' <br>about checking out our office too?</p></div>'
+                ['<div class="infobox"><h4>La Curva</h4><p>Visitanos dentro del food park' +
+                ' <br>Pv en Puerto Vallarta</p></div>'
                     , latitude, longitude, 2]
             ];
 
